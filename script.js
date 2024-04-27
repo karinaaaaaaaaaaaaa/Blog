8c3f4c19973c2a201b92958e7bcd4d3f504debc9
karinaaaaaaaaaaaaa <167879974+karinaaaaaaaaaaaaa@users.noreply.github.com>
2024-04-27 18:42:57 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+function openarticles()
+{
+    window.location.href="index.html";
+}
+
+function openreviews()
+{
+    window.location.href="reviews.html";
+}
+
+function openmedia()
+{
+    window.location.href="media.html";
+}
